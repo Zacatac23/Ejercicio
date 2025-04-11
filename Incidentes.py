190d88b5cf4f4ec3c6b61e526cf8dcceb120c7b2
Zacatac23 <zac231104@uvg.edu.gt>
2025-04-10 19:29:35 -0600

    quitar archivos

--- Incidentes.py
+++
@@ -1,6 +0,0 @@
-# Incidentes.py
-
-incidentes = [
-    {"id":"1","name": "juan", "reporte": "Mitelefono no funciona", "status": "pendiente", "fecha": "2021-10-10"},
-    {"id":"2","name": "maria", "reporte": "Mi computadora no arranca", "status": "resuelto", "fecha": "2021-10-11"},
-]
